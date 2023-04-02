bccbd9bf9b8ae77bf28c7524741ecf41f6af00f4
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-04-02 23:08:45 +0600

    Create Pointer related.cpp

---
+++ Pointer related.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a=10;
+    int *pt;///making a pointer variable
+    pt=&a;///akon a er address take pt er modde reke dilam tahole a akon pt ta a ke point korbe
+    cout<<a<<endl;///
+    cout<<pt<<endl;///10 er address ta aikane dekabe
+    cout<<*pt<<endl;///10 manta aikane dekabe
+
+/*Jkoni amra akta pointer variable declare korlam pt name and pt er modde amra a er address take reke dilam and a ke
+print kora mane 10 print and pt print mane a er address take print kora and *pt print hole a er manta print hbe*/
+
+}
